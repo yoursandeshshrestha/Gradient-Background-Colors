5cfb94cd954a35d4f2f96fa56b978133c403ae6d
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-03-26 20:51:12 +0530

    added one more feature.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button1 = document.querySelector(".button1");
 const button2 = document.querySelector(".button2");
+const button3 = document.querySelector(".button3");
 let text = document.querySelector(".text");
 
 let rgb1 = "#000";
@@ -27,8 +28,15 @@ const clickbutton2 = () => {
     text.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
 }
 
+const clickbutton3 = () => {
+    rgb1 = hexColor();
+    rgb2 = hexColor();
+    document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    text.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+}
 
 
 
 button1.addEventListener('click', clickbutton1);
 button2.addEventListener('click', clickbutton2);
+button3.addEventListener('click', clickbutton3);
